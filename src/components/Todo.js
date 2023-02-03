8fe566ff3dca0156b77b736d6f714cccc6fb592f
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-02-04 00:58:38 +0530

    commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,14 @@
- import React from 'react';
+import React from 'react';
 
- export const Todo  = () =>{
-    
- }
+export const Todo  = (props) =>{
+    return(
+        <>
+            <div className="todo" id="todo-${id}">
+                {props.title}
+                <div className ="todo-status">
+                    {props.completed}
+                </div>
+            </div>
+        </>
+    )
+}
