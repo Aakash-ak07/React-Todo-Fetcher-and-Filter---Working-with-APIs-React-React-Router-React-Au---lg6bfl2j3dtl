9a5c7d291d77f98148c206682fb4095366ea43c5
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-02-04 01:07:25 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 const apiLink = "https://jsonplaceholder.typicode.com/todos";
 
 const App = () => {
+
   // const [complete, setcomplete] = useState(true);
   const [ischecked, setischecked] = useState(true);
   const [incomplete, setincomplete] = useState(false);
@@ -15,45 +16,53 @@ const App = () => {
   const [get, setget] = useState([]);
   // let first;
 
-    useEffect(()=>{
-        
-        async function getStoredData(){
-            const response = await axios.get(apiLink);
-            console.log(response);
-            // console.log(response.data);
-            
-            setpost(response.data.splice(0, 20));
-            // console.log(...post)
-            setloading(false);
-            // const timer = setTimeout(() => {<Loader />}, 1000);
-            //   return () => clearTimeout(timer);
-        }
-        getStoredData();
-    }, [])
   
-    function handleChange(e){
-        let word = e.target.value;
-        setincomplete(!incomplete);
-        setischecked(!ischecked);
+  useEffect(()=>{
     
-        // if(word === "completed"){
-        //   const filtered = post.filter(item => item==="false")
-        //   setpost(filtered)
-        //   console.log(word);
-        // }
+    async function getStoredData(){
+        const response = await axios.get(apiLink);
+        console.log(response);
+        // console.log(response.data);
+        
+        setpost(response.data.splice(0, 20));
+        // console.log(...post)
+        setloading(false);
+        // const timer = setTimeout(() => {<Loader />}, 1000);
+        //   return () => clearTimeout(timer);
     }
+    getStoredData();
+  }, [])
+  
+  function handleChange(e){
+    //  setcomplete(!complete);
 
-    return (
+    let word = e.target.value;
+    
+    // console.log(word)
+    setincomplete(!incomplete);
+    setischecked(!ischecked);
+  
+    // if(word === "completed"){
+    //   const filtered = post.filter(item => item==="false")
+    //   setpost(filtered)
+    //   console.log(word);
+    // }
+  }
+
+  return (
     <>
       {loading && (
         <h1>
-            <Loader />
+          <Loader />
         </h1>
       )}
       { !loading && (
         <>
-          {post.map((e)=>(
+          {post
+          .map((e)=>(
+            
             <Todo
+            
                 key = {e.id}
                 title = {e.title}
                 completed = {e.completed ? "completed" : "incomplete"}
@@ -69,6 +78,7 @@ const App = () => {
             } */}
             <br />
             <br />
+              {/* <Todo id = {response.data[0].id}/> */}
             <div id='filter-holder'>
               <span>Show completed</span>
                 <input type="checkbox" 
@@ -88,6 +98,6 @@ const App = () => {
       )
       }
     </>
-    )
-}
+  )
+  }
 export default App;
